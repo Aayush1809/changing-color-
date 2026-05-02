05c35b02c252d504d3afd4eb72d47500a695630a
Aayush1809 <aayush181809@gmail.com>
2026-05-02 09:18:08 +0530

    Add files via upload

---
+++ just.js
@@ -0,0 +1,16 @@
+let a = prompt("enter name of the color ")
+if (a==="red"){
+    document.body.style.backgroundColor = "red"
+}
+else if(a==="blue"){
+    document.body.style.backgroundColor = "blue"
+}
+else if(a==="black"){
+    document.body.style.backgroundColor = "black"
+}
+else if(a==="green"){
+    document.body.style.backgroundColor = "green"
+}
+else if(a==="pink"){
+    document.body.style.backgroundColor = "pink"
+}
